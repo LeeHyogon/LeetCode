739d5c104fcf74c28a23b8e543bcc8a37b54973b
LeeHyogon <45483116+LeeHyogon@users.noreply.github.com>
2026-02-23 20:46:05 +0900

    Time: 10 ms (5.39%), Space: 19.7 MB (26.99%) - LeetHub

---
+++ 1475-final-prices-with-a-special-discount-in-a-shop/1475-final-prices-with-a-special-discount-in-a-shop.py
@@ -0,0 +1,29 @@
+from collections import deque
+class Solution:
+    def finalPrices(self, prices: List[int]) -> List[int]:
+        q = deque()
+        ans = [-1] * len(prices)
+
+        for idx, p in enumerate(prices):
+
+            while q and q[-1][1] >= p:
+                t_idx, t_val = q.pop()
+                ans[t_idx] = t_val - p
+                print(t_idx, t_val, p)
+
+            q.append((idx, p))
+    
+        #last element
+        if q:
+            t_idx, t_val = q.pop()
+            ans[t_idx] = t_val
+        
+        for i in range(len(prices)):
+            if ans[i] == -1:
+                ans[i] = prices[i]
+        
+        return ans
+            
+
+
+        
